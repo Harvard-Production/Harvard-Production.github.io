568dbacfe672a691519a3570b8cf76968c7d1292
Corey Adams <coreyjadams@gmail.com>
2018-03-13 14:47:20 -0400

    Updating.

--- update.sh
+++ update.sh
@@ -11,6 +11,7 @@ cd /n/holylfs/LABS/guenette_lab/production/Harvard-Production.github.io/
 # Then, it builds the html, commits the changes, and pushes to master.
 
 # Make the html table for file status:
+rm -f harvard_projects_summary.html
 summarize_projects.py
 
 # Create the dataset html page:
@@ -21,11 +22,12 @@ touch tempDatasets.md
 echo "title: Datasets" >> tempDatasets.md
 echo "Last updated: $(date)" >> tempDatasets.md
 
-cat harvard-production-summary.html >> tempDatasets.md
+cat harvard_projects_summary.html >> tempDatasets.md
 
 mv tempDatasets.md content/pages/Datasets.md
 
 make html
-# git add pages/datasets.html
-# git commit -m "Update datasets $(date)"
-# git push
+mv output/pages/datasets.html pages/
+git add pages/datasets.html
+git commit -m "Update datasets $(date)"
+git push
